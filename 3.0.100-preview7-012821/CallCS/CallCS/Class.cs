85958ba9c6b04947b24c377c75d9df1a64d792b5
Akira Kawamata <autumn@piedey.co.jp>
2019-07-26 11:29:13 +0900

    add preview7 samples

---
+++ 3.0.100-preview7-012821/CallCS/CallCS/Class.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+public class JavaScriptInvokable
+{
+    [JSInvokable]
+    public static Task<string> myfunc()
+    {
+        return Task.FromResult("from C#");
+    }
+}
