65d52eb593e325790fec9d774a0981fa0f22cd03
Akira Kawamata <autumn@piedey.co.jp>
2019-11-07 16:06:57 +0900

    reconstruct tree

---
+++ oldSamples/3.0.100/PageToPage/PageToPage/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PageToPage
+{
+    public class Class
+    {
+        static public int publicCount = 0;
+    }
+}
