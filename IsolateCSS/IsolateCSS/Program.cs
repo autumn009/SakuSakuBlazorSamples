3d5f293ad39c5b22a62914f2bfa6f36a8a5f7b6e
Akira Kawamata <autumn@piedey.co.jp>
2020-08-26 11:50:54 +0900

    add IsolateCSS

--- IsolateCSS/IsolateCSS/Program.cs
+++ IsolateCSS/IsolateCSS/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace ComponentCSS
+namespace IsolateCSS
 {
     public class Program
     {
