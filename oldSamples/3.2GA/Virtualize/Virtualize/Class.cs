30335d92fdb41a8ec7bcf047c201b54aabe33598
Akira Kawamata <autumn@piedey.co.jp>
2020-11-16 12:07:05 +0900

    update for .NET 5 GA

---
+++ oldSamples/3.2GA/Virtualize/Virtualize/Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Virtualize
+{
+    public static class Class
+    {
+        public static IEnumerable<char> HugeList = sub();
+        private static int counter = 0;
+
+        private static IEnumerable<char> sub()
+        {
+            for (int i = 32; i < 32767; i++)
+            {
+                if( i == 256)
+                    yield return counter.ToString()[0];
+
+                yield return (char)i;
+            }
+        }
+        public static void IncrementCount()
+        {
+            counter++;
+        }
+    }
+}
